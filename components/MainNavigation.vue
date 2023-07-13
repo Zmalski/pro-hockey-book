59d0ddb3997a973c64f304f53035afabee0b19d0
Zach <zacharymaludzinski@gmail.com>
2023-07-12 21:48:21 -0700

    desktop nav

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -13,39 +13,54 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <v-list-item
-            prepend-icon="mdi-home"
-            title="Home"
-            value="home"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-account-multiple"
-            title="Players"
-            value="players"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-hockey-sticks"
-            title="Teams"
-            value="teams"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-chart-line"
-            title="Stats"
-            value="stats"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-podium"
-            title="Records"
-            value="records"
-          ></v-list-item>
+          <nuxt-link to="/" class="nav-item">
+            <v-list-item
+              prepend-icon="mdi-home"
+              title="Home"
+              value="home"
+            ></v-list-item
+          ></nuxt-link>
+          <nuxt-link to="/players" class="nav-item">
+            <v-list-item
+              prepend-icon="mdi-account-multiple"
+              title="Players"
+              value="players"
+            ></v-list-item
+          ></nuxt-link>
+          <nuxt-link to="/teams" class="nav-item">
+            <v-list-item
+              prepend-icon="mdi-hockey-sticks"
+              title="Teams"
+              value="teams"
+            ></v-list-item>
+          </nuxt-link>
+          <nuxt-link to="/stats" class="nav-item">
+            <v-list-item
+              prepend-icon="mdi-chart-line"
+              title="Stats"
+              value="stats"
+            ></v-list-item>
+          </nuxt-link>
+          <nuxt-link to="/records" class="nav-item">
+            <v-list-item
+              prepend-icon="mdi-podium"
+              title="Records"
+              value="records"
+            ></v-list-item>
+          </nuxt-link>
         </v-list>
       </v-navigation-drawer>
 
-      <v-main style="height: 250px"></v-main>
+      <v-main style="height: 250px"><slot></slot></v-main>
     </v-layout>
   </v-card>
 </template>
 <script>
 export default {};
 </script>
-<style lang="css"></style>
+<style lang="css">
+  .nav-item {
+    text-decoration: none;
+    color: inherit; /* or any color value you prefer */
+  }
+</style>
