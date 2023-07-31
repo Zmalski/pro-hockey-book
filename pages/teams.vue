9e3900be9f9c1c70eeff1f028820a00b3b80a5dd
Zmalski <zacharymaludzinski@gmail.com>
2023-07-31 02:57:08 -0700

    test firestore/API implementation

--- pages/teams.vue
+++ pages/teams.vue
@@ -2,32 +2,38 @@
   <div>
     Hello teams!
     <div v-if="isLoading">Loading...</div>
-    <div v-else-if="teamData?.teams?.length === 0">No teams found.</div>
-    <div v-else>Found {{ teamData?.teams?.length }} teams.
-      <div v-for="team in teamData.teams" :key="team.id">
+    <div v-else-if="teams?.length === 0">No teams found.</div>
+    <div v-else>Found {{ teams?.length }} teams.
+      <div v-for="team in teams" :key="team.id">
         {{ team.name }}
       </div>
     </div>
 
     <div>The end!</div>
+    <v-btn @click="triggerAddUser">Add User</v-btn>
+    <v-btn @click="fetchTeams">Fetch Teams</v-btn>
   </div>
 </template>
 <script>
 export default {
   name: 'Teams',
   data: () => ({
-    isLoading: true,
-    teamData: [],
+    teams: [],
   }),
   methods: {
     async fetchTeams() {
-      const { data } = await useFetch('/api/nhl/teams')
-      return data
+      const { getTeams } = useNHL() // auto-imported
+      this.teams = await getTeams()
+    },
+    async triggerAddUser() {
+      const { addUser } = useFirestore() // auto-imported
+      await addUser('Z', 'Y', 2000)
     },
   },
-  async mounted() {
-    this.teamData = await this.fetchTeams()
-    this.isLoading = false
+  computed: {
+    isLoading() {
+      return this.teams.length === 0;
+    }
   },
 }
 </script>
