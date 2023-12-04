8bd7acad01b06fa64f636a1b76f0670107409175
Zmalski <zacharymaludzinski@gmail.com>
2023-12-04 00:37:40 -0800

    add firebase integration

--- plugins/01.firebase.ts
+++ plugins/01.firebase.ts
@@ -2,32 +2,41 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  initializeFirestore,
+  getFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+} from "firebase/firestore";
 
-export default defineNuxtPlugin(nuxtApp => {
-    const config = useRuntimeConfig()
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
 
-    const firebaseConfig = {
-        apiKey: config.public.firebaseApiKey,
-        authDomain: config.public.firebaseAuthDomain,
-        projectId: config.public.firebaseProjectId,
-        storageBucket: config.public.firebaseStorageBucket,
-        messagingSenderId: config.public.firebaseMessagingSenderId,
-        appId: config.public.firebaseAppId,
-        measurementId: config.public.firebaseMeasurementId,
-    };
+  const firebaseConfig = {
+    apiKey: config.public.firebaseApiKey,
+    authDomain: config.public.firebaseAuthDomain,
+    projectId: config.public.firebaseProjectId,
+    storageBucket: config.public.firebaseStorageBucket,
+    messagingSenderId: config.public.firebaseMessagingSenderId,
+    appId: config.public.firebaseAppId,
+    measurementId: config.public.firebaseMeasurementId,
+  };
 
-    const app = initializeApp(firebaseConfig)
+  const app = initializeApp(firebaseConfig);
 
-    // const analytics = getAnalytics(app)
-    const auth = getAuth(app)
-    const firestore = getFirestore(app)
-    nuxtApp.vueApp.provide('auth', auth)
-    nuxtApp.provide('auth', auth)
+  // const analytics = getAnalytics(app)
+  const auth = getAuth(app);
+  const firestore = initializeFirestore(app, {
+    localCache: persistentLocalCache(
+      /*settings*/ { tabManager: persistentMultipleTabManager() }
+    ),
+  });
+  nuxtApp.vueApp.provide("auth", auth);
+  nuxtApp.provide("auth", auth);
 
-    nuxtApp.vueApp.provide('firestore', firestore)
-    nuxtApp.provide('firestore', firestore)
+  nuxtApp.vueApp.provide("firestore", firestore);
+  nuxtApp.provide("firestore", firestore);
 
-    // nuxtApp.vueApp.provide('analytics', analytics)
-    // nuxtApp.provide('analytics', analytics)
-})
+  // nuxtApp.vueApp.provide('analytics', analytics)
+  // nuxtApp.provide('analytics', analytics)
+});
